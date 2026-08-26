f9e2b996b355af6eea61324c138f08d13daddff4
Adam Menczykowski <adam@youi.design>
2026-08-26 15:02:02 +0100

    add fathom

--- app/app.vue
+++ app/app.vue
@@ -14,49 +14,57 @@ useHead({
   link: [
     { rel: 'icon', type: 'image/png', href: '/favicon.png' }
   ],
-  script: [{
-    key: 'identity-jsonld',
-    type: 'application/ld+json',
-    innerHTML: JSON.stringify({
-      '@context': 'https://schema.org',
-      '@graph': [{
-        '@type': 'Person',
-        '@id': personId,
-        'name': 'Adam Menczykowski',
-        'url': global.siteUrl,
-        'image': `${global.siteUrl}${global.picture.light}`,
-        'jobTitle': ['Design Engineer', 'Full-Stack Developer', 'Technical Director'],
-        'homeLocation': {
-          '@type': 'Place',
-          'name': 'Hitchin, Hertfordshire, UK'
-        },
-        'worksFor': {
-          '@type': 'Organization',
-          'name': 'You I Limited',
-          'url': 'https://youi.design'
-        },
-        'sameAs': [
-          'https://github.com/admench',
-          'https://x.com/adammench'
-        ],
-        'knowsAbout': [
-          'Product design',
-          'Web application development',
-          'Laravel',
-          'Statamic',
-          'Vue.js',
-          'Nuxt'
-        ]
-      }, {
-        '@type': 'WebSite',
-        '@id': `${global.siteUrl}/#website`,
-        'url': global.siteUrl,
-        'name': 'Adam Menczykowski',
-        'inLanguage': 'en-GB',
-        'publisher': { '@id': personId }
-      }]
-    })
-  }],
+  script: [
+    {
+      'key': 'fathom-analytics',
+      'src': 'https://cdn.usefathom.com/script.js',
+      'data-site': 'YPFIIWEZ',
+      'defer': true
+    },
+    {
+      key: 'identity-jsonld',
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify({
+        '@context': 'https://schema.org',
+        '@graph': [{
+          '@type': 'Person',
+          '@id': personId,
+          'name': 'Adam Menczykowski',
+          'url': global.siteUrl,
+          'image': `${global.siteUrl}${global.picture.light}`,
+          'jobTitle': ['Design Engineer', 'Full-Stack Developer', 'Technical Director'],
+          'homeLocation': {
+            '@type': 'Place',
+            'name': 'Hitchin, Hertfordshire, UK'
+          },
+          'worksFor': {
+            '@type': 'Organization',
+            'name': 'You I Limited',
+            'url': 'https://youi.design'
+          },
+          'sameAs': [
+            'https://github.com/admench',
+            'https://x.com/adammench'
+          ],
+          'knowsAbout': [
+            'Product design',
+            'Web application development',
+            'Laravel',
+            'Statamic',
+            'Vue.js',
+            'Nuxt'
+          ]
+        }, {
+          '@type': 'WebSite',
+          '@id': `${global.siteUrl}/#website`,
+          'url': global.siteUrl,
+          'name': 'Adam Menczykowski',
+          'inLanguage': 'en-GB',
+          'publisher': { '@id': personId }
+        }]
+      })
+    }
+  ],
   htmlAttrs: {
     lang: 'en-GB'
   }
