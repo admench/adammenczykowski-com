632d24833591f0996086d3031a6953ab2c448e68
Adam Menczykowski <adam@youi.design>
2026-08-28 10:47:59 +0100

    blog seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,9 +28,10 @@ export default defineNuxtConfig({
       routes: [
         '/',
         '/about/',
-        '/links'
+        '/links',
+        '/blog'
       ],
-      crawlLinks: false
+      crawlLinks: true
     }
   },
 
